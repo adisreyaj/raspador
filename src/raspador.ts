573f728871eb3eb472b6e85a37944a3dbd1a7042
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-03-29 19:24:42 +0530

    chore: add documentation
    Added comments and documentation for PR, Bug fixes etc

--- src/raspador.ts
+++ src/raspador.ts
@@ -1,26 +1,32 @@
-import { MetaData, Selectors } from '@interface/interfaces';
+import { MetaData, Root, Selectors } from '@interface/interfaces';
 import { loadHTML } from '@utils/load-html';
 import { runSelectors } from '@utils/selectors';
 
+/**!
+ *  Initialize `raspador` with the html to scrape
+ * @param - the html string
+ */
 export default (html: string): ((selectors: Selectors) => MetaData | null) => {
   const $ = loadHTML(html);
-  return (selectors: Selectors): MetaData | null => {
-    const metadata = selectors($);
-    const keys = Object.keys(metadata);
-    if (keys?.length > 0) {
-      return keys.reduce((acc, curr) => {
-        let value = null;
-        try {
-          value = runSelectors(metadata[curr]);
-        } catch (error) {
-          value = null;
-        }
-        return {
-          ...acc,
-          [curr]: value,
-        };
-      }, {});
-    }
-    return null;
-  };
+  return computeValues($);
+};
+
+const computeValues = ($: Root) => (selectors: Selectors): MetaData | null => {
+  const metadata = selectors($);
+  const keys = Object.keys(metadata);
+  if (keys?.length > 0) {
+    return keys.reduce((acc, curr) => {
+      let value = null;
+      try {
+        value = runSelectors(metadata[curr]);
+      } catch (error) {
+        value = null;
+      }
+      return {
+        ...acc,
+        [curr]: value,
+      };
+    }, {});
+  }
+  return null;
 };
--- src/utils/load-html.ts
+++ src/utils/load-html.ts
@@ -1,3 +1,3 @@
+import { Root } from '@interface/interfaces';
 import cheerio from 'cheerio';
-import { Root } from '../interface/interfaces';
 export const loadHTML = (html: string): Root => cheerio.load(html);
